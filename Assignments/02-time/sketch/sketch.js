330c6b903127b968ae9337a60fbcadbd22e4fa1c
ame-bui <66383380+ame-bui@users.noreply.github.com>
2021-11-07 10:41:36 -0500

    update 1107

---
+++ Assignments/02-time/sketch/sketch.js
@@ -0,0 +1,15 @@
+let faceapi;
+let dectections = [];
+
+let video; 
+let canvas;
+
+function setup(){
+    canvas = createCanvas(480,360);
+    video = createCapture(VIDEO);
+    video.size(width,height);
+}
+
+function draw(){
+
+}
